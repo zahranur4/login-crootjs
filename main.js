f4c7371494d70db05981b35ad24f6f808cd2dc0d
Zahra Nur'azijah Lutfiani <zahranurazijah23@gmail.com>
2026-04-20 06:17:22 +0700

    login dengan google cloud dan crootjs

---
+++ main.js
@@ -0,0 +1,25 @@
+import { postJSON } from "https://cdn.jsdelivr.net/gh/crootjs/api@0.0.1/api.js";
+import { setInner } from "https://cdn.jsdelivr.net/gh/crootjs/element@0.0.1/element.js";
+
+window.handleCredentialResponse = (response) => {
+    console.log("ID Token dari Google: " + response.credential);
+
+    const backendUrl = "https://your-app.alwaysdata.net/login"; 
+    
+    postJSON(backendUrl, "Authorization", "token-bebas", response, (result) => {
+        console.log("Respon dari Backend:", result);
+        setInner("hasil", `Status: ${result.status} - Selamat datang!`);
+    });
+};
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "belum_ada.apps.googleusercontent.com",
+        callback: window.handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large", width: "250" }
+    );
+};
